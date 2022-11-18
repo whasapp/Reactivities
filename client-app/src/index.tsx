9e7fbc4905f91f69ce4525ffc9bb601c69308020
whasapp <filipeisakovcr7@gmail.com>
2022-11-18 18:26:58 +0000

    End of Section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './App/layout/styles.css';
 import App from './App/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './App/stores/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <StoreContext.Provider value={store}>
     <App />
+  </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
